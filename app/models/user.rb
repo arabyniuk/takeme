0188a6418c4b226fda13c6d45227e7024c027f21
Konstantin S Kazarin <zul@telemiks.tv>
2016-05-08 02:40:14 +1000

    typo fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # 以下を追記
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", small:"45x45>"}, storage: :s3, bucket: S3_BUCKET,
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", small:"45x45>"}, storage: :s3, bucket: S3_BUCKET
   validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
 
   has_many :tweets
