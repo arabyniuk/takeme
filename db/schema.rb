aeba531f97b555a3b011847da992db7a93dadd7b
TakemuraTatsuya <tatsuya1970@TakemuruyanoMBP.lan>
2016-06-19 21:56:33 +0900

    change files

--- db/schema.rb
+++ db/schema.rb
@@ -46,10 +46,10 @@ ActiveRecord::Schema.define(version: 20160504105608) do
     t.datetime "updated_at",                                        null: false
     t.text     "introduction",           limit: 65535
     t.string   "name",                   limit: 255
-    t.string   "avatar_file_name",       limit: 255
-    t.string   "avatar_content_type",    limit: 255
-    t.integer  "avatar_file_size",       limit: 4
-    t.datetime "avatar_updated_at"
+    t.string   "image_file_name",       limit: 255
+    t.string   "image_content_type",    limit: 255
+    t.integer  "image_file_size",       limit: 4
+    t.datetime "image_updated_at"
     t.string   "fb",                     limit: 255
     t.string   "place",                  limit: 255
     t.string   "language1",              limit: 255
