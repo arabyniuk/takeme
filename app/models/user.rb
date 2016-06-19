bdb0618dcbf82aa72fb13580469fafa16d315aa7
Alexander Zagaynov <zalex80@gmail.com>
2016-06-19 13:47:00 +0300

    default avatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_many :comments
 
-  has_attached_file :image, styles: { medium:"300x300>", thumb:"150x150>", small:"45x45>"},
+  has_attached_file :avatar, styles: { medium:"300x300>", thumb:"150x150>", small:"45x45>"},
   :storage =>:s3,
   :bucket => S3_BUCKET,
   :path => ":attachment/:id/:style.:extension",
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
   #:url  => "/brali-image/images/:id/:style/:extension"
 
   # validates_attachment_content_type :image, content_type: ["image/jpg","image/jpeg","image/png"]
-  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 
 
