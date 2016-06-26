acf00bbec64bc3043bd05e97ec4837d8a567327b
TakemuraTatsuya <tatsuya1970@TakemuruyanoMBP.lan>
2016-06-26 16:32:47 +0900

    change files

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Takeme
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.initialize_on_precompile = false #herokuはこれがいる
+
+
   end
 end
