ef560c118f688ac23cf594bb934b88948dde285f
Airat Shigapov <contact@airatshigapov.com>
2016-10-09 13:02:33 +0300

    Fix message sending

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,26 +1,23 @@
 class MessagesController < ApplicationController
 
- def index
-
-
+  def index
+    @user = User.find(params[:user_id])
   end
 
-
   def create
+    @message = Message.create(message: params[:message], user_id: params[:user_id])
 
-   @message = Message.create(message: params[:message], user_id: params[:user_id])
+    user = User.find(params[:user_id])
 
-   #user = User.find(params[:user_id])
-
-   redirect_to "/users/#{@message.user_id}/messages"
+    redirect_to "/users/#{@message.user_id}/messages"
 
 
     #メールのメッセージ（後で考える）
     #NoticeMailer.sendmail_confirm(@comment.tweet.id,tweet.user_id,current_user.id).deliver
-   end
-
+  end
 
   private
+
   def comment_params
     params.permit(:message, :user_id)
   end
